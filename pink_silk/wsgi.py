93982be0de9f7f38d5d40e24dd387e16e9a7e1a2
rlorimier <rere.rnt@gmail.com>
2022-10-24 13:48:11 +0000

    first commit

---
+++ pink_silk/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pink_silk project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pink_silk.settings')
+
+application = get_wsgi_application()
