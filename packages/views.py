c21c3457d51dc4e04796635360a9aa5d3fd52dbe
rlorimier <rere.rnt@gmail.com>
2022-11-08 19:09:42 +0000

    added packages view and template

--- packages/views.py
+++ packages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Packages
 
 # Create your views here.
+def all_packages(request):
+    """A view to show all the package options available"""
+
+    packages = Packages.objects.all()
+
+    context = {
+        'packages': packages,
+    }
+
+    return render(request, 'packages.html', context)
