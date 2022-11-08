dd2a252dcee484439648b58defb82ab626b64fe6
rlorimier <rere.rnt@gmail.com>
2022-11-08 18:57:25 +0000

    customized admin

--- packages/admin.py
+++ packages/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Packages
 
 # Register your models here.
-admin.site.register(Packages)
+class PackagesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'duration',
+    )
+
+    ordering = ('-price',)
+
+admin.site.register(Packages, PackagesAdmin)
--- packages/models.py
+++ packages/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Packages(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Packages'
+
     name = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
