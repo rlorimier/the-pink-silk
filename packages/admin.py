f3d52c7d94ff806386dc0ed3d15591afd1d7d9e7
rlorimier <rere.rnt@gmail.com>
2022-11-08 18:43:15 +0000

    added packages model

--- packages/admin.py
+++ packages/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Packages
 
 # Register your models here.
+admin.site.register(Packages)
--- packages/models.py
+++ packages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Packages(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    duration = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
