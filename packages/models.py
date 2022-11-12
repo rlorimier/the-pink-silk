97d168790b26b57acf7ff81fea48bc1f8b0a8254
rlorimier <rere.rnt@gmail.com>
2022-11-12 18:07:45 +0000

    added more itens on fixtures and models

--- packages/models.py
+++ packages/models.py
@@ -8,9 +8,20 @@ class Packages(models.Model):
 
     name = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     duration = models.TextField()
     description = models.TextField()
+    included1 = models.TextField(null=True, blank=True)
+    included2 = models.TextField(null=True, blank=True)
+    included3 = models.TextField(null=True, blank=True)
+    treatments1 = models.TextField(null=True, blank=True)
+    treatments2 = models.TextField(null=True, blank=True)
+    treatments3 = models.TextField(null=True, blank=True)
+    treatments4 = models.TextField(null=True, blank=True)
+    treatments5 = models.TextField(null=True, blank=True)
+    treatments6 = models.TextField(null=True, blank=True)
+    treatments7 = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
