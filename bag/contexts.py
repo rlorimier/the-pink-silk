8ea5269be60bfe76cc16eb0ed4cabaeb9c69c4db
rlorimier <rere.rnt@gmail.com>
2022-11-12 23:31:14 +0000

    added bag contexts

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from packages.models import Packages
 
 
 def bag_contents(request):
 
     bag_items = []
-    total = 0
+    grand_total = total = 0
     package_count = 0
+
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        package = get_object_or_404(Packages, pk=item_id)
+        total += quantity * package.price
+        package_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'package': package,
+        })
 
     grand_total = total
 
