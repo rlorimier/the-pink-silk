b952dea12d52491f18052104309d7a7207a8efa7
rlorimier <rere.rnt@gmail.com>
2022-11-15 23:27:25 +0000

    added forms, signals and views

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ChackoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
