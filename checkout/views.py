a57c7919b4adf5726cdb75eb0191de5950ed1e16
rlorimier <rere.rnt@gmail.com>
2022-11-16 16:10:29 +0000

    added checkout views, urls and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.success(
+            request, "There's nothing in your bag at the moment"
+            )
+        return redirect(reverse('packages'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
