9c29aa2a29474fdeacc4192291e307b5ba17e8cc
rlorimier <rere.rnt@gmail.com>
2022-11-17 20:13:33 +0000

    added basic checkout functionality

--- pink_silk/settings.py
+++ pink_silk/settings.py
@@ -164,6 +164,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
