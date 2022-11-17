8eef7f3cab322c3c5cf3213e34a7c86c527d8d28
rlorimier <rere.rnt@gmail.com>
2022-11-17 23:01:49 +0000

    added class methods to webhook handlers

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path(
         'checkout_success/<order_number>',
         views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
--- pink_silk/settings.py
+++ pink_silk/settings.py
@@ -168,6 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
