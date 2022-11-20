ed47967edc38706409781b2af29430099cbd2a8b
rlorimier <rere.rnt@gmail.com>
2022-11-20 18:43:22 +0000

    added delete packages functionality

--- packages/urls.py
+++ packages/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.all_packages, name='packages'),
     path('<int:package_id>', views.package_detail, name='package_detail'),
     path('edit/<int:package_id>/', views.edit_package, name='edit_package'),
+    path(
+        'delete/<int:package_id>/',
+        views.delete_package, name='delete_package'),
     path('add/', views.add_package, name='add_package'),
 ]
--- packages/views.py
+++ packages/views.py
@@ -33,9 +33,10 @@ def add_package(request):
     if request.method == 'POST':
         form = PackagesForm(request.POST, request.FILES)
         if form.is_valid():
+            package = form.save()
             form.save()
             messages.success(request, 'Successfully added new package!')
-            return redirect(reverse('add_package'))
+            return redirect(reverse('package_detail', args=[package.id]))
         else:
             messages.success(
                 request,
@@ -75,3 +76,11 @@ def edit_package(request, package_id):
     }
 
     return render(request, template, context)
+
+
+def delete_package(request, package_id):
+    """ Delete a package """
+    package = get_object_or_404(Packages, pk=package_id)
+    package.delete()
+    messages.success(request, 'Package deleted!')
+    return redirect(reverse('packages'))
