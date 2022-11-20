6065034f587cab2d3d30e737254c859bb25cf361
rlorimier <rere.rnt@gmail.com>
2022-11-20 20:38:25 +0000

    set secret key

--- pink_silk/settings.py
+++ pink_silk/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*d@r4s0ye2j==!ywvfx41%zuo&la!)i&h^+)58sy$to@xux&#p'
+SECRET_KEY = OS.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-pink-silk.herokuapp.com', 'localhost']
 
