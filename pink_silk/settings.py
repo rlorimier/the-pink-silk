33f4dbce9df26bf194eef8971af43d27fab31f5e
rlorimier <rere.rnt@gmail.com>
2022-11-22 13:17:31 +0000

    deployment commit

--- pink_silk/settings.py
+++ pink_silk/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environrue
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-pink-silk.herokuapp.com', 'localhost']
 
